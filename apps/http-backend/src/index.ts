dde481eac21feb1b74e9894e9288aaaa283b6ab8
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-13 23:48:54 +0530

    zod schema added

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,20 +1,38 @@
-import express,{Response,Request} from "express";
+import express, { Response, Request } from "express";
 import { middleware, AuthRequest } from "./middleware";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
+import {
+  CreateRoomSchema,
+  CreateUserSchema,
+  SigninSchema,
+} from "@repo/common/types";
 
-const app = express()
+const app = express();
 app.use(express.json());
-const PORT = 3000
+const PORT = 3000;
 
-
-app.post("/signup", (req:Request,res:Response)=> {
+app.post("/signup", (req: Request, res: Response) => {
+  const data = CreateUserSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
   //db call
   res.status(200).json({
     roomId: 123,
   });
-})
+});
 app.post("/signin", (req: Request, res: Response) => {
+  const data = SigninSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({
+      message: "Incorrect Inputs",
+    });
+    return;
+  }
   const userId = 1;
   const token = jwt.sign(
     {
@@ -25,14 +43,19 @@ app.post("/signin", (req: Request, res: Response) => {
   res.status(200).json({ token });
 });
 app.post("/room", middleware, (req: AuthRequest, res: Response) => {
+  const data = CreateRoomSchema.safeParse(req.body);
+  if (!data.success) {
+    res.json({ message: "Incorrect Inputs" });
+    return;
+  }
   //db call
   const userId = req.user?.userId;
   res.status(200).json({
-    message:`User ${userId} joined the room `,
-    roomId: 123
-  })
+    message: `User ${userId} joined the room `,
+    roomId: 123,
+  });
 });
 
-app.listen(PORT,() => {
-  console.log(`Server running on ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
--- apps/http-backend/src/middleware.ts
+++ apps/http-backend/src/middleware.ts
@@ -1,35 +1,38 @@
-import { Request, Response,NextFunction } from "express";
-import { JWT_SECRET } from "./config";
+import { Request, Response, NextFunction } from "express";
+import { JWT_SECRET } from "@repo/backend-common";
 import jwt from "jsonwebtoken";
 
-interface JwtPayload{
-    userId: string;
+interface JwtPayload {
+  userId: string;
 }
-export interface AuthRequest extends Request{
-    user?: JwtPayload;
+export interface AuthRequest extends Request {
+  user?: JwtPayload;
 }
 
-export function middleware(req:AuthRequest,res:Response, next:NextFunction){
-    const authHeader = req.headers.authorization;
+export function middleware(
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
 
-    if(!authHeader){
-        res.status(401).json({message:"Authorization token Missing"});
-        return
+  if (!authHeader) {
+    res.status(401).json({ message: "Authorization token Missing" });
+    return;
+  }
+  const token = authHeader?.split(" ")[1];
+  if (!token) {
+    res.status(401).json({ messgae: "Token missing" });
+    return;
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
+    if (typeof decoded == "string") {
+      return;
     }
-    const token = authHeader?.split(" ")[1];
-    if(!token){
-        res.status(401).json({messgae:"Token missing"});
-        return
-    }
-    try {
-        
-        const decoded = jwt.verify(token, JWT_SECRET) as JwtPayload;
-        if (typeof decoded == "string") {
-          return;
-        }
-        req.user = decoded;
-        next();
-    } catch (error) {
-        res.status(403).json({messgae:"Invalid or expired token",error})
-    }
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(403).json({ messgae: "Invalid or expired token", error });
+  }
 }
